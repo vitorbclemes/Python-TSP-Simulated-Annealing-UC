33d3d92fe7b4a8521bed90cdb4e63a297e621d6a
vitorbclemes <vitorbc.oficial@gmail.com>
2022-11-14 10:11:18 -0300

    finished

---
+++ utils.py
@@ -0,0 +1,27 @@
+def load_runs(fp: str):
+    runs = []
+    with open(fp, 'r') as file:
+        for line in file.readlines():
+            runs.append(float(line))
+
+        return runs
+
+def load_convergence(fp: str, max_itens: int = 100006):
+
+    iters = []
+    temps = []
+    dists = []
+    with open(fp, "r") as txt_file:
+        iter = 1
+        for line in txt_file.readlines():
+            raw = line.split(" ")
+            temp = raw[0]
+            dist = raw[1]
+
+            iters.append(int(iter))
+            temps.append(float(temp))
+            dists.append(float(dist))
+
+            iter += 1
+
+    return iters[:max_itens], temps[:max_itens], dists[:max_itens]
